c39a6773aa38636b610140754f89063462a9eef2
Cristiano Henrique <cristianovisk@gmail.com>
2023-03-03 14:42:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,32 +5,32 @@ function pause(){
 }
 
 
-echo "Executando: curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -"
+echo 'Executando: curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -'
 pause 'Press [Enter]...'
 curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
 clear
 
 echo "Executando: deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list"
 pause 'Press [Enter]...'
-echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
+echo 'deb https://download.falco.org/packages/deb stable main | tee -a /etc/apt/sources.list.d/falcosecurity.list'
 clear
 
-echo "Executando: apt-get update -y"
+echo 'Executando: apt-get update -y'
 pause 'Press [Enter]...'
 apt-get update -y
 clear
 
-echo "Executando: apt install -y dkms make clang llvm dialog linux-headers-$(uname -r)"
+echo 'Executando: apt install -y dkms make clang llvm dialog linux-headers-$(uname -r)'
 pause 'Press [Enter]...'
-apt install -y dkms make clang llvm dialog linux-headers-$(uname -r) 
+apt install -y dkms make clang llvm dialog linux-headers-$(uname -r)
 clear
 
-echo "Executando: apt-get install -y falco"
+echo 'Executando: apt-get install -y falco'
 pause 'Press [Enter]...'
 apt-get install -y falco
 clear
 
-echo "Executando: falco-driver-loader module"
+echo 'Executando: falco-driver-loader module'
 pause 'Press [Enter]...'
 falco-driver-loader module
 clear
