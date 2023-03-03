8e64ab8168f8639125ee2c0f0df09ef467ca3812
Cristiano Henrique <cristianovisk@gmail.com>
2023-03-03 14:49:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 clear
 function pause(){
+   echo 'Press [Enter]...'
    read -p "$*"
 }
 
 
 echo 'Executando: curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -'
-pause 'Press [Enter]...'
+pause
 curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
 clear
 
 echo 'Executando: deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list'
-pause 'Press [Enter]...'
+pause
 echo 'deb https://download.falco.org/packages/deb stable main | tee -a /etc/apt/sources.list.d/falcosecurity.list'
 clear
 
 echo 'Executando: apt-get update -y'
-pause 'Press [Enter]...'
+pause
 apt-get update -y
 clear
 
 echo 'Executando: apt install -y dkms make clang llvm dialog "linux-headers-$(uname -r)"'
-pause 'Press [Enter]...'
+pause
 apt install -y dkms make clang llvm dialog "linux-headers-$(uname -r)"
 clear
 
 echo 'Executando: apt-get install -y falco'
-pause 'Press [Enter]...'
+pause
 apt-get install -y falco
 clear
 
 echo 'Executando: falco-driver-loader module'
-pause 'Press [Enter]...'
+pause
 falco-driver-loader module
 clear
 
 echo 'Executando: wget https://raw.githubusercontent.com/cristianovisk/install_falco_ubuntu/main/falco.yaml -O /etc/falco/falco.yaml'
-pause 'Press [Enter]...'
+pause
 wget https://raw.githubusercontent.com/cristianovisk/install_falco_ubuntu/main/falco.yaml -O /etc/falco/falco.yaml
